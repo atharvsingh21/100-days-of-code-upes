04bfb2555ac1fb5c4557e1f5989d2fcc4cdbf218
Atharv <atharvsingh2108@gmail.com>
2026-09-16 18:11:56 +0530

    Day 38: solved problem 75 and 76

---
+++ day38/day38_Q76.c
@@ -0,0 +1,34 @@
+//Check if a matrix is symmetric.
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of square matrix (n x n): ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isSymmetric = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (isSymmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
