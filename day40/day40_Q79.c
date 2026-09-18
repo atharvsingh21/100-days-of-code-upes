62545a202039d90f5223f8c8f323a37232f85017
Atharv <atharvsingh2108@gmail.com>
2026-09-18 10:08:40 +0530

    Day 40:solved problem 79 and 80

---
+++ day40/day40_Q79.c
@@ -0,0 +1,32 @@
+//Perform diagonal traversal of a matrix.
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Diagonal traversal:\n");
+    for (int d = 0; d < rows + cols - 1; d++) {
+        for (int i = 0; i < rows; i++) {
+            int j = d - i;
+            if (j >= 0 && j < cols) {
+                printf("%d ", matrix[i][j]);
+            }
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
