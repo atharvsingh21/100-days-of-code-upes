9974d11fc27d9040fc0d721fd704f3c3f7fa772d
Atharv <atharvsingh2108@gmail.com>
2026-09-21 16:16:25 +0530

    Day 43: solved problem 85 and 86

---
+++ day43/day43_Q86.c
@@ -0,0 +1,34 @@
+//Check if a string is a palindrome.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str);
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    int isPalindrome = 1;
+    int start = 0, end = len - 1;
+    while (start < end) {
+        if (str[start] != str[end]) {
+            isPalindrome = 0;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    if (isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
